2d4cf464d93ebca8a934777531c590762f4dd97b
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2024-05-08 20:04:06 +0530

    test updated

--- src/flags.jsx
+++ src/flags.jsx
@@ -1,39 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import "./flags.css"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./flags.css";
 
 const Title = ({ flagUrl, name, altflag }) => {
-    return (
-        <div className='imgbox'>
-            <img src={flagUrl} alt={altflag} className='img' />
-            <h2>{name}</h2>
-        </div>
-    )
-}
+  return (
+    <div className="imgbox">
+      <img src={flagUrl} alt={altflag} className="img" />
+      <h2>{name}</h2>
+    </div>
+  );
+};
 export default function Flags() {
-    const [countries, setCountries] = useState([]);
-    async function getUser() {
-        try {
-            const response = await axios.get('https://restcountries.com/v3.1/all');
-            setCountries(response.data);
-        } catch (error) {
-            console.error(error);
-        }
+  const [countries, setCountries] = useState([]);
+  async function getUser() {
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
+  useEffect(() => {
+    getUser();
+  }, []);
+  console.log({ countries });
 
-    useEffect(() => {
-        getUser()
-    }, [])
-    console.log({ countries })
-
-
-    return (
-        <div className='maindiv'>
-            {countries.map((country)=>(
-                
-                <Title flagUrl={country.flags.png} name={country.name.common} altFlag={country.flags.alt}/>
-            ))}
-        </div>
-    )
+  return (
+    <div className="maindiv">
+      {countries.map((country) => (
+        <Title
+        key={country.cca3}
+          flagUrl={country.flags.png}
+          name={country.name.common}
+          altflag={country.flags.alt}
+        />
+      ))}
+    </div>
+  );
 }
